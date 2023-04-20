25d2278a480d9ba8f75ff36ef4bc8f9adc482b4d
marie taylor <marietaylortailor@hotmail.com>
2023-04-20 22:56:57 +0100

    alternate alternatingCase code3

--- toAlternatingCase.js
+++ toAlternatingCase.js
@@ -11,21 +11,21 @@
 
 // console.log("hello WORLD".toAlternatingCase());
 
-String.prototype.toAlternatingCase = function () {
-  let chars = this.split("");
-  chars.forEach((char, index) => {
-    chars[index] =
-      char.toUpperCase() === char ? char.toLowerCase() : char.toUpperCase();
-  });
-  return chars.join("");
-};
-
-console.log("BoNjOuR WORLD".toAlternatingCase());
-
 // String.prototype.toAlternatingCase = function () {
-//   return this.split("")
-//     .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
-//     .join("");
+//   let chars = this.split("");
+//   chars.forEach((char, index) => {
+//     chars[index] =
+//       char.toUpperCase() === char ? char.toLowerCase() : char.toUpperCase();
+//   });
+//   return chars.join("");
 // };
 
-// console.log("hello WORLD".toAlternatingCase());
+// console.log("BoNjOuR WORLD".toAlternatingCase());
+
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
+};
+
+console.log("HOWDY WORLD".toAlternatingCase());
