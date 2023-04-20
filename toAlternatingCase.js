c17fba90d8534901d5737f4090a40d84760079f5
marie taylor <marietaylortailor@hotmail.com>
2023-04-20 22:56:01 +0100

    alternate alternatingCase code

--- toAlternatingCase.js
+++ toAlternatingCase.js
@@ -1,3 +1,16 @@
+// String.prototype.toAlternatingCase = function () {
+//   let chars = this.split("");
+//   for (let i = 0; i < chars.length; i++) {
+//     chars[i] =
+//       chars[i].toUpperCase() === chars[i]
+//         ? chars[i].toLowerCase()
+//         : chars[i].toUpperCase();
+//   }
+//   return chars.join("");
+// };
+
+// console.log("hello WORLD".toAlternatingCase());
+
 String.prototype.toAlternatingCase = function () {
   let chars = this.split("");
   chars.forEach((char, index) => {
@@ -7,7 +20,7 @@ String.prototype.toAlternatingCase = function () {
   return chars.join("");
 };
 
-console.log("bonJOuR WORLD".toAlternatingCase());
+console.log("BoNjOuR WORLD".toAlternatingCase());
 
 // String.prototype.toAlternatingCase = function () {
 //   return this.split("")
