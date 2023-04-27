8dcebd2f1136ba3e7296bd644076050f70e88a5a
marie taylor <marietaylortailor@hotmail.com>
2023-04-27 22:10:22 +0100

    use of mocking to simulate random Math.random

--- raceDay.js
+++ raceDay.js
@@ -1,5 +1,11 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
+const registeredEarly = true;
+const runnersAge = 19;
+if (registeredEarly && runnersAge > 18) {
+  raceNumber += 1000;
+}
+
 module.exports = {
   generateRaceNumber: function () {
     return raceNumber;
