3b284a2c7f453a0a291406ba0239d2853aaa0753
marie taylor <marietaylortailor@hotmail.com>
2023-05-03 11:37:40 +0100

    plantNeedsConditional should accept string redFail test

--- plantNeedsConditional.test.js
+++ plantNeedsConditional.test.js
@@ -3,3 +3,11 @@ const plantNeedsWater = require("./plantNeedsConditional");
 test("if it is Wednesday, the plant needs to be watered", () => {
   expect(plantNeedsWater("Wednesday")).tobe(true);
 });
+
+// With this test, we're ensuring that our plantNeedsWater function only accepts a string parameter, and will throw an error if it's called with any other type of data.
+
+test("plantNeedsWater should accept a string parameter", () => {
+  expect(() => {
+    plantNeedsWater(123);
+  }).toThrow();
+});
