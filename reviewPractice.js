3c88833343ef7c70374e6dc3a43d457867e67fe5
marie taylor <marietaylortailor@hotmail.com>
2023-05-03 23:06:05 +0100

    working tests - error not thrown for sting

--- reviewPractice.js
+++ reviewPractice.js
@@ -1,5 +1,19 @@
-const spellChecker = function (sentence) {
-  if (typeof sentence !== "string") throw new Error("Input must be a string");
+const spellChecker = function (sentence, wordBank) {
+  if (typeof sentence !== "string") {
+    throw new Error("Input must be a string");
+  }
+
+  const words = sentence.split(" ");
+  const newSentence = [];
+  const checkedWords = words.map((word) => {
+    if (wordBank.includes(word)) {
+      newSentence.push(`~${word}~`);
+    } else {
+      newSentence.push(word);
+    }
+  });
+
+  return newSentence.join(" ");
 };
 
 module.exports = spellChecker;
--- reviewPractice.test.js
+++ reviewPractice.test.js
@@ -5,5 +5,23 @@ test("should throw error if input is not a string", () => {
 });
 
 test("should not throw error if input is a string", () => {
-  expect(() => spellChecker("This is a string")).not.toThrow();
+  expect(() => spellChecker("This is a string", [])).not.toThrow();
+});
+
+test("should replace misspelled word with a ~word~", () => {
+  const wordBank = [
+    "apple",
+    "banana",
+    "orange",
+    "pear",
+    "kiwi",
+    "grape",
+    "melon",
+    "peach",
+    "mango",
+    "lemon",
+  ];
+  expect(spellChecker("I like to eat appel and peache", wordBank)).toBe(
+    "I like to eat ~appel~ and ~peache~"
+  );
 });
