3dfaf626f0fa63bbc445306f1466734cb3a01c06
marie taylor <marietaylortailor@hotmail.com>
2023-05-09 21:27:03 +0100

    Passing test edge case single string input

--- sameCase.test.js
+++ sameCase.test.js
@@ -1,6 +1,18 @@
 const sameCase = require("./sameCase");
 
 test("should throw error if input is not a string", () => {
+  expect(() => sameCase()).toThrow(
+    "Input should contain two single string characters"
+  );
+});
+
+test("should throw error if only one input is not a string", () => {
+  expect(() => sameCase(1)).toThrow(
+    "Input should contain two single string characters"
+  );
+});
+
+test("should throw error if input is not a string", () => {
   expect(() => sameCase(1, 2)).toThrow(
     "Input should contain two single string characters"
   );
