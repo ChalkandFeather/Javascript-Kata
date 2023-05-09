7cb6557b7dbc98109f1a749c4b31d63371bc0aea
marie taylor <marietaylortailor@hotmail.com>
2023-05-09 15:24:49 +0100

    Passing Odd test

--- oddOrEven.test.js
+++ oddOrEven.test.js
@@ -3,3 +3,7 @@ const oddOrEven = require("./oddOrEven");
 test("should return even for a even integer", () => {
   expect(oddOrEven(2)).toBe("Even");
 });
+
+test("should return even for a even integer", () => {
+  expect(oddOrEven(1)).toBe("Odd");
+});
