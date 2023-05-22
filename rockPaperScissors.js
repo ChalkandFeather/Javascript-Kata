4e51bf591b56fa30d7a3d627f9cca98c117ea553
marie taylor <marietaylortailor@hotmail.com>
2023-05-22 13:34:46 +0100

    Steps 1-4 rockpaperscissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,14 @@
-const getUserChoice = (userInput) => {};
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("error");
+  }
+};
+
+console.log(getUserChoice("paper"));
