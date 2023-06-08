4b516e606f7fd9d1d7f1fd680453efb15dd0be76
marie taylor <marietaylortailor@hotmail.com>
2023-06-08 16:21:34 +0100

    step 5&6

--- whaleTalk.js
+++ whaleTalk.js
@@ -1,4 +1,10 @@
 const input = "my name is Moby Dick";
-const vowels = [a, e, i, o, u];
+const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
-for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {}
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  // console.log(`inputIndex is ${inputIndex}`);
+}
+
+// if (inputIndex === vowels) {
+//   resultArray.push;
