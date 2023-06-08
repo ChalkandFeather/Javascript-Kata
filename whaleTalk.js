a0a1d4267f16e01d1566ea627b7d82b72a371694
marie taylor <marietaylortailor@hotmail.com>
2023-06-08 16:23:27 +0100

    step 7

--- whaleTalk.js
+++ whaleTalk.js
@@ -4,6 +4,9 @@ const resultArray = [];
 
 for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
   // console.log(`inputIndex is ${inputIndex}`);
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
+    console.log(`vowelsIndex is ${vowelsIndex}`);
+  }
 }
 
 // if (inputIndex === vowels) {
