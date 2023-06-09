8385e29fe2404428e9925a2b40d5737250d8c6ef
marie taylor <marietaylortailor@hotmail.com>
2023-06-09 11:04:18 +0100

    step11&12

--- whaleTalk.js
+++ whaleTalk.js
@@ -1,8 +1,14 @@
-const input = "my name is Moby Dick";
+const input = "Your name is Moby Dick";
 const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
 
 for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  if (input[inputIndex] === "e") {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === "u") {
+    resultArray.push(input[inputIndex]);
+  }
   // console.log(`inputIndex is ${inputIndex}`);
   for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
     // console.log(`vowelsIndex is ${vowelsIndex}`);
