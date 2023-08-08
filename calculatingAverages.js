a4bd1a56c05c96c012e071086c4224d5ddfaecc9
marie taylor <marietaylortailor@hotmail.com>
2023-08-08 22:38:45 +0100

    Function corrected to enable Calculator object to access average

--- calculatingAverages.js
+++ calculatingAverages.js
@@ -1,14 +1,17 @@
-function Calculator() {
-  if (arguments.length === 0) {
-    return 0; // If no arguments, return 0
-  } else {
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-      total += arguments[i]; // Add up all the arguments
-    }
+var Calculator = {
+  average: function () {
+    if (arguments.length === 0) {
+      return 0; // If no arguments, return 0
+    } else {
+      let total = 0;
 
-    return total / arguments.length; // Calculate and return the average
-  }
-}
+      for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i]; // Add up all the arguments
+      }
+
+      return total / arguments.length; // Calculate and return the average
+    }
+  },
+};
 
 module.exports = Calculator;
